067e9a4869688cb3df7db67e17d9982bd2daee58
cschweda <cschweda@gmail.com>
2022-11-14 16:29:41 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt3-content-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-lvjgwk-fa3wpx)
---
+++ pages/[...slug].vue
@@ -0,0 +1,14 @@
+akh
+<template>
+  <main>
+    <ContentRenderer :value="page" />
+  </main>
+</template>
+
+<script setup>
+const route = useRoute();
+const { data: page } = await useAsyncData(
+  'page-data-' + route.params.slug,
+  () => queryContent('/', ...route.params.slug).findOne()
+);
+</script>
